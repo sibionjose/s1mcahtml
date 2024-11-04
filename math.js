8efc56dc82453d71c5f43b65e7bc7d3ef14a9fa4
sibionjose <loviyonjosh2200@gmail.com>
2024-11-04 15:15:33 +0530

    Add files via upload

---
+++ math.js
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+   <head>
+      <title>Adding two numbers using JavaScript in HTML</title>
+      <script>
+         var num1 = 10;
+         var num2 = 40;
+         var sum = num1+num2;
+         document.write("Sum of two numbers is " + sum);
+      </script>
+   </head>
+   <body></body>
+</html>
